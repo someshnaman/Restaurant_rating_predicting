ed710f11507d018f5e9e247791a0830cf40e5998
someshnaman <66522077+someshnaman@users.noreply.github.com>
2021-11-29 00:33:57 +0530

    Added Procfile and ran app.run in main

--- main.py
+++ main.py
@@ -59,8 +59,8 @@ port = int(os.getenv("PORT", 5001))
 
 if __name__ == "__main__":
     host = '0.0.0.0'
-    # app.run()
-    httpd = simple_server.make_server(host=host, port=port, app=app)
+    app.run()
+    # httpd = simple_server.make_server(host=host, port=port, app=app)
     # httpd = simple_server.make_server(host='127.0.0.1', port=5000, app=app)
     print("Serving on %s %d" % (host, port))
-    httpd.serve_forever()
+    # httpd.serve_forever()
